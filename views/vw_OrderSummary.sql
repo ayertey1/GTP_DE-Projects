7cb9da1113624c0e4b41d3399dea6c2f436c9626
ayertey1 <peter.ayertey@amalitechtraining.org>
2025-04-16 18:52:21 +0000

    view creation for low stock and order summary

---
+++ views/vw_OrderSummary.sql
@@ -0,0 +1,15 @@
+CREATE VIEW vw_OrderSummary AS
+SELECT 
+    o.OrderID,
+    c.CustomerName,
+    o.OrderDate,
+    o.TotalAmount,
+    COUNT(od.ProductID) AS TotalItems
+FROM 
+    Orders o
+JOIN 
+    Customers c ON o.CustomerID = c.CustomerID
+JOIN 
+    OrderDetails od ON o.OrderID = od.OrderID
+GROUP BY 
+    o.OrderID, c.CustomerName, o.OrderDate, o.TotalAmount;
